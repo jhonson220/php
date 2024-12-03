30278cf1b26f56ddc80082cec4e09a1630a566a8
jhonson220 <micamidesmelo@gmail.com>
2024-12-03 11:53:13 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>api animales</title>
+</head>
+<body>
+<form action="" method="POST" class="w-50 mx-auto 2-p">
+    <div class="text-center">
+        nombre:
+         <label for="nombre"Nombre="nombre" class="form-label">nombre</label>
+         <input type="text"  class="form-label" id="nombre" name="nombre" required><br><br>
+    </div>
+    <div class="mb-3">
+         <label for="especie"Nombre="especie" class="form-label">especie</label>
+         <input type="text"  class="form-label" id="especie" name="especie" required><br><br>
+         </div>
+         <div class="mb-3">
+         <label for="edad"Nombre="edad" class="form-label">edad</label>
+         <input type="text"  class="form-label" id="edad" name="edad" required><br><br>
+         </div>
+         <div class="mb-3">
+         <label for="habitat"Nombre="habitat" class="form-label">habitat</label>
+         <input type="text"  class="form-label" id="habitat" name="habitat" required><br><br>
+         </div>     
+    <input type="submit" value="registar">
+</form>
+</body>
+</html>
